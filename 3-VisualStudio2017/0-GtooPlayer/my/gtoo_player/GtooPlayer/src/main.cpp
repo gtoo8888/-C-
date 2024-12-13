819d2caf56aac6419250d9eb646913f8634b61f0
gtoo8888 <ujm456@126.com>
2024-12-13 23:54:00 +0800

    feat: add xiaomiwatch sleep data

--- 3-VisualStudio2017/0-GtooPlayer/my/gtoo_player/GtooPlayer/src/main.cpp
+++ 3-VisualStudio2017/0-GtooPlayer/my/gtoo_player/GtooPlayer/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QtWidgets/QApplication>
 #include "GtooPlayer.h"
+#include "XiaomiWatchDisplayWdg.h"
 #include "RemWordWdg.h"
 #include "gtest/gtest.h"
 // #include <QTest>
@@ -13,9 +14,10 @@ int main(int argc, char *argv[]) {
     //GtooPlayer w;
     //w.show();
 
-    RemWordWdg* remWordWdg = new RemWordWdg();
-    remWordWdg->show();
+    //RemWordWdg* remWordWdg = new RemWordWdg();
+    //remWordWdg->show();
 
-
+    XiaomiWatchDisplayWdg* miWdg = new XiaomiWatchDisplayWdg();
+    miWdg->show();
     return a.exec();
 }
